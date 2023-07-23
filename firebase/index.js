d4dd31db41a78b9a541a76443028d26f66e46d63
amang8775 <bhaiyakaku18@gmail.com>
2023-07-23 15:45:48 +0530

    error fixed

--- firebase/index.js
+++ firebase/index.js
@@ -3,14 +3,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBbBfji3B3BaXEhquYngitnH52YxCBknJs",
-  authDomain: "mylist1-2fad5.firebaseapp.com",
-  projectId: "mylist1-2fad5",
-  storageBucket: "mylist1-2fad5.appspot.com",
-  messagingSenderId: "472105994452",
-  appId: "1:472105994452:web:dfcab9842dee958681d341",
-  measurementId: "G-C6HR4SW2CW",
+  apiKey: "AIzaSyAxU5cnIFRlJNCEerjNOv0m9DiI1QPc7P8",
+  authDomain: "temporary-file-d74ca.firebaseapp.com",
+  projectId: "temporary-file-d74ca",
+  storageBucket: "temporary-file-d74ca.appspot.com",
+  messagingSenderId: "623752506126",
+  appId: "1:623752506126:web:9a06112c22dc8d9443455f",
+  measurementId: "G-H008GH6GJ0"
 };
+
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
